fd4e8e0cd89977be6acb566335e36e361cbd778a
Jere <jere.niemi@solteq.com>
2025-09-21 00:02:13 +0300

    Add User Administration and Edit User pages with user management functionality

---
+++ Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,16 @@
+using PiikkiTracker.Data;
+
+namespace PiikkiTracker.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllUsersAsync();
+        Task<ApplicationUser> GetUserByIdAsync(string userId);
+        Task<ApplicationUser> UpdateUserAsync(ApplicationUser user);
+        Task<bool> DeleteUserAsync(string userId);
+        Task<IEnumerable<ApplicationUser>> SearchUsersAsync(string searchTerm);
+        Task<bool> DeleteAllUserJobsAsync(string userId);
+        Task<bool> DeleteAllUserProductsAsync(string userId);
+        Task<bool> ResetUserDataAsync(string userId);
+    }
+}
